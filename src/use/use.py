c9ff0ecbf43208f19be1bc49faa61b835279abd9
Anselm Kiefner <use-github@anselm.kiefner.de>
2021-07-11 23:27:51 +0200

    hash_algo: Hash annotation

--- src/use/use.py
+++ src/use/use.py
@@ -77,38 +77,23 @@ import time
 import traceback
 import zipfile
 import zipimport
-
-from collections import defaultdict
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 from enum import Enum
-from functools import singledispatch
-from functools import update_wrapper
-from functools import wraps
+from functools import singledispatch, update_wrapper, wraps
 from importlib import metadata
 from importlib.machinery import EXTENSION_SUFFIXES
-from logging import DEBUG
-from logging import StreamHandler
-from logging import getLogger
-from logging import root
+from logging import DEBUG, StreamHandler, getLogger, root
 from pathlib import Path
 from types import ModuleType
-from typing import Callable
-from typing import Dict
-from typing import List
-from typing import Optional
-from typing import Tuple
-from typing import Union
+from typing import Callable, Dict, List, Optional, Tuple, Union
 from warnings import warn
 
 import mmh3
 import requests
 import toml
-
 from packaging import tags
 from packaging.specifiers import SpecifierSet
-from packaging.version import LegacyVersion
-from packaging.version import Version
-from packaging.version import parse
+from packaging.version import LegacyVersion, Version, parse
 from yarl import URL
 
 __version__ = "0.3.2"
@@ -767,7 +752,7 @@ To safely reproduce: use(use.URL('{url}'), hash_algo=use.{hash_algo}, hash_value
                 version:str=None, 
                 initial_globals:dict=None, 
                 auto_install:bool=False, 
-                hash_algo:str=Hash.sha256, 
+                hash_algo:Hash=Hash.sha256, 
                 hash_value:str=None,
                 default=mode.fastfail,
                 aspectize=None,
