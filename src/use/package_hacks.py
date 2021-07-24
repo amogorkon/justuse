47df389980ab6e5830d07ae517a7ea89a18ce677
Anselm Kiefner <use-github@anselm.kiefner.de>
2021-07-24 19:06:23 +0200

    black formatting

--- src/use/package_hacks.py
+++ src/use/package_hacks.py
@@ -29,20 +29,27 @@ from packaging.specifiers import SpecifierSet
 import use
 
 root.addHandler(StreamHandler(sys.stderr))
-if "DEBUG" in os.environ: root.setLevel(DEBUG)
+if "DEBUG" in os.environ:
+    root.setLevel(DEBUG)
 log = getLogger(__name__)
 
-def readstring(path, lines=False, /, encoding="ISO-8859-1", 
-    raw_lines=False):
+
+def readstring(path, lines=False, /, encoding="ISO-8859-1", raw_lines=False):
     mode = "rb" if encoding is None else "r"
-    with open(path, mode, buffering=-1,
-        encoding=encoding, newline=("\x0a" if encoding else None)) as f:
+    with open(
+        path,
+        mode,
+        buffering=-1,
+        encoding=encoding,
+        newline=("\x0a" if encoding else None),
+    ) as f:
         if lines:
             if raw_lines:
                 return f.readlines()
             else:
                 return list(map(str.rstrip, f.readlines()))
         return f.read()
+
 
 def remove_cached_module(module_name):
     module_to_del = []
@@ -51,9 +58,9 @@ def remove_cached_module(module_name):
         module_to_del.append(part)
         module_key = ".".join(module_to_del)
         if module_key in sys.modules:
-            log.info("Deleting sys.modules[%s]",
-                repr(module_key))
+            log.info("Deleting sys.modules[%s]", repr(module_key))
             del sys.modules[module_key]
+
 
 def create_solib_links(archive: zipfile.ZipFile, folder: Path):
     log.debug(f"create_solib_links({archive=}, {folder=})")
@@ -72,14 +79,18 @@ def create_solib_links(archive: zipfile.ZipFile, folder: Path):
         split_on = [".python", ".cpython", ".cp"]
         simple_name, os_ext = None, EXTENSION_SUFFIXES[-1]
         for s in split_on:
-            if not s in sofile.name: continue
+            if not s in sofile.name:
+                continue
             simple_name = sofile.name.split(s)[0]
-        if simple_name is None: continue
+        if simple_name is None:
+            continue
         link = Path(sofile.parent / f"{simple_name}{os_ext}")
-        if link == sofile: continue
+        if link == sofile:
+            continue
         log.debug(f"{link=}, {sofile=}")
         link.unlink(missing_ok=True)
         link.symlink_to(sofile)
+
 
 def save_module_info(package_name, rdists, version, url, path, that_hash, folder):
     if package_name not in rdists:
@@ -88,16 +99,19 @@ def save_module_info(package_name, rdists, version, url, path, that_hash, folder
         rdists[package_name][version] = {}
     # Update package version metadata
     assert url is not None
-    rdists[package_name][version].update({
-        "package": package_name,
-        "version": version,
-        "url": url.human_repr(),
-        "path": str(path) if path else None,
-        "folder": folder.absolute().as_uri(),
-        "filename": path.name,
-        "hash": that_hash
-    })
+    rdists[package_name][version].update(
+        {
+            "package": package_name,
+            "version": version,
+            "url": url.human_repr(),
+            "path": str(path) if path else None,
+            "folder": folder.absolute().as_uri(),
+            "filename": path.name,
+            "hash": that_hash,
+        }
+    )
     use.persist_registry()
+
 
 def ensure_extracted(path, folder, url=None):
     if not folder.exists():
@@ -115,11 +129,24 @@ def ensure_extracted(path, folder, url=None):
                 file.extractall(folder)
                 create_solib_links(file, folder)
 
+
 @use.register_hack("numpy", specifier=SpecifierSet(">=1.0"))
-def numpy(*, package_name, rdists, version, url, path, that_hash, folder, fatal_exceptions, module_name):
+def numpy(
+    *,
+    package_name,
+    rdists,
+    version,
+    url,
+    path,
+    that_hash,
+    folder,
+    fatal_exceptions,
+    module_name,
+):
     log.debug("hacking numpy!")
     ensure_extracted(path, folder, url)
-    if sys.path[0] != "": sys.path.insert(0, "")
+    if sys.path[0] != "":
+        sys.path.insert(0, "")
     original_cwd = Path.cwd()
     mod = None
     try:
@@ -131,26 +158,39 @@ def numpy(*, package_name, rdists, version, url, path, that_hash, folder, fatal_
         remove_cached_module(module_name)
         os.chdir(original_cwd)
 
+
 @use.register_hack("protobuf", specifier=SpecifierSet(">=1.0"))
-def protobuf(*, package_name, rdists, version, url, path, that_hash, folder, fatal_exceptions, module_name):
+def protobuf(
+    *,
+    package_name,
+    rdists,
+    version,
+    url,
+    path,
+    that_hash,
+    folder,
+    fatal_exceptions,
+    module_name,
+):
     log.debug("hacking protobuf!")
     ensure_extracted(path, folder, url)
-    if sys.path[0] != "": sys.path.insert(0, "")
+    if sys.path[0] != "":
+        sys.path.insert(0, "")
     original_cwd = Path.cwd()
     mod = None
     try:
         os.chdir(folder)
         # needed because protobuf corrupts sys.path to looking here
-        tgt = use.home / Path(
-            f".local/lib/python3.{sys.version_info[1]}/site-packages")
+        tgt = use.home / Path(f".local/lib/python3.{sys.version_info[1]}/site-packages")
         log.info("folder=%s, symlink_to(tgt=%s)", folder, tgt)
         # remove any existing symlink here in case we already
         # loaded a different version
         tgt.parent.mkdir(mode=0o755, parents=True, exist_ok=True)
         tgt.unlink(missing_ok=True)
         tgt.symlink_to(folder.absolute())
-        pth_src = "\n\n".join(readstring(str(pth_path)) \
-            for pth_path in folder.glob("*.pth"))
+        pth_src = "\n\n".join(
+            readstring(str(pth_path)) for pth_path in folder.glob("*.pth")
+        )
         log.info("pth_src=[%s]", pth_src)
         # variable is needed as protobuf assumes it will be in stack
         sitedir = str(folder)
