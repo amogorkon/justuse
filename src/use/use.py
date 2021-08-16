f9d99e07c381ca0946347b9f087870e673a7d33d
David Reilly <greyblue9@gmail.com>
2021-08-16 04:47:43 -0400

    Clean up alias initializer

--- src/use/use.py
+++ src/use/use.py
@@ -1722,6 +1722,6 @@ if not test_version:
     sys.modules["use"] = use
 
 # no circular import this way
-hacks_path = Path(Path(__file__).parent, "package_hacks.py")
+hacks_path = Path(__file__).parent / "package_hacks.py"
 assert hacks_path.exists()
 use(hacks_path)  # type: ignore
