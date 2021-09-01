f6bd40d75fbbbb7150696a92842804310e0aafa9
Anselm Kiefner <use-github@anselm.kiefner.de>
2021-09-01 13:19:09 +0200

    #202 done

--- tests/.tests/.test2.py
+++ tests/.tests/.test2.py
@@ -1,13 +1,2 @@
-from packaging.version import Version as PkgVersion
-class Version(PkgVersion):    
-    def __init__(self, versionstr, *, major=0, minor=0, patch=0):
-        if major or minor or patch:
-            # string as only argument, no way to construct a Version otherwise - WTF
-            return super().__init__(".".join((str(major), str(minor), str(patch))))
-        if isinstance(versionstr, str):
-            return super().__init__(versionstr)
-        else:
-            return super().__init__(str(versionstr))  # this is just wrong :|
+import hashlib
 
-    def __iter__(self):
-        yield from self.release
