cff2570e8ca99401c3df0e2c3ad5d4c2840374e6
ghandic <andy.challis@capgemini.com>
2021-10-05 15:02:00 +1100

    Added some draft scripts for mass testing

---
+++ tests/mass/justtest.py
@@ -0,0 +1,61 @@
+import sys
+import json
+from typing import List, Optional
+import re
+import logging
+
+import use
+from pydantic import BaseModel
+
+logging.root.setLevel(logging.ERROR)
+
+
+class PackageToTest(BaseModel):
+    name: str
+    versions: List[str]
+    repo: Optional[str] = None
+    stars: Optional[int] = None
+
+
+class Packages(BaseModel):
+    data: List[PackageToTest] = []
+
+    def append(self, item: PackageToTest) -> None:
+        self.data.append(item)
+
+
+with open("pypi.json", "r") as f:
+    packages = Packages(data=json.load(f)["data"])
+
+packages.data.sort(key=lambda p: p.stars or 0, reverse=True)
+
+# Use to test the below code for now
+# packages = Packages(data=[PackageToTest(name="sqlalchemy", versions=["1.4.10"])])
+
+failed_packages = []
+for i, package in enumerate(packages.data):
+    try:
+        use(package.name, version=package.versions[-1], modes=use.auto_install)
+    except RuntimeWarning as e:
+        if str(e).startswith("Failed to auto-install "):
+            hashes = re.findall("hashes={([a-z0-9A-Z', ]+)}", str(e))[0]
+            hashes = {_hash.strip("'") for _hash in hashes.split(", ")}
+    try:
+        module = use(package.name, version=package.versions[-1], modes=use.auto_install, hashes=hashes)
+        assert module
+
+    except Exception as e:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        failed_packages.append(
+            {
+                "name": package.name,
+                "version": package.versions[-1],
+                "stars": package.stars,
+                "err": [exc_type, exc_value, exc_tb],
+            }
+        )
+
+    # if i > 10:
+    #     break
+
+print(failed_packages)
