2deeb118954e65711a8fd294ae847ffe687f7519
Anselm Kiefner <use-github@anselm.kiefner.de>
2021-10-07 22:29:49 +0200

    Version moved to version_stuff.py

---
+++ src/use/version_stuff.py
@@ -0,0 +1,47 @@
+from packaging.version import Version as PkgVersion
+
+#%% Version and Packaging
+
+# Well, apparently they refuse to make Version iterable, so we'll have to do it ourselves.
+# This is necessary to compare sys.version_info with Version and make some tests more elegant, amongst other things.
+
+
+class Version(PkgVersion):
+    def __new__(cls, *args, **kwargs):
+        if args and isinstance(args[0], Version):
+            return args[0]
+        else:
+            return super(cls, Version).__new__(cls)
+
+    def __init__(self, versionstr=None, *, major=0, minor=0, patch=0):
+        if isinstance(versionstr, Version):
+            return
+        if not (versionstr or major or minor or patch):
+            raise ValueError(
+                "Version must be initialized with either a string or major, minor and patch"
+            )
+        if major or minor or patch:
+            # string as only argument, no way to construct a Version otherwise - WTF
+            return super().__init__(".".join((str(major), str(minor), str(patch))))
+        return super().__init__(versionstr)
+
+    def __iter__(self):
+        yield from self.release
+
+    def __repr__(self):
+        return (
+            'use.Version("'
+            + ".".join(
+                map(
+                    str,
+                    (
+                        *self.release[0:-1],
+                        str(self.release[-1]) + self.pre[0] + str(self.pre[1]),
+                    ),
+                )
+            )
+            + '")'
+        )
+
+    def __hash__(self):
+        return hash(self._version)
