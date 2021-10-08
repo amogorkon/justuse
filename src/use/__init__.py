36a62e33577d433a085105803d13def9c3380b78
David Reilly <greyblue9@users.noreply.github.com>
2021-10-08 03:21:11 -0400

    Remove typeguard hook

--- src/use/__init__.py
+++ src/use/__init__.py
@@ -1,7 +1,8 @@
 try:
-    from typeguard.importhook import install_import_hook
-
-    install_import_hook(packages=["use"])
+    # from typeguard.importhook import install_import_hook
+    # TODO: Figure out how we might be able to re-enable at a later date
+    pass
+    # install_import_hook(packages=["use"])
 except ImportError:
     # typeguard not installed
     pass
