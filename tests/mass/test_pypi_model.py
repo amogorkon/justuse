bb9d2ad3e67d01018e25595230ec75a8365e9418
ghandic <andy.challis@capgemini.com>
2021-10-08 12:25:38 +1100

    Added example of using pydantic

--- tests/mass/test_pypi_model.py
+++ tests/mass/test_pypi_model.py
@@ -4,16 +4,21 @@ import traceback
 
 import requests
 
-from pypi_model import Package
+from pypi_model import PyPI_Project
 
 with open("pypi.json", "r") as f:
     packages = json.load(f)
 
 
-for package in packages["data"]:
+for i, package in enumerate(packages["data"]):
     r = requests.get(f"https://pypi.org/pypi/{package['name']}/json")
     try:
-        Package(**r.json())
+        project = PyPI_Project(**r.json())
+        filtered = project.filter_by_version_and_current_platform(project.info.version)
+        for version in filtered.releases.values():
+            for release in version:
+                print(release.filename)
+                print(release.justuse)
     except:
         exc_type, exc_value, _ = sys.exc_info()
         tb = traceback.format_exc()
@@ -25,3 +30,5 @@ for package in packages["data"]:
         print(json.dumps(fail, indent=2, sort_keys=True))
         break
 
+    if i > 4:
+        break
