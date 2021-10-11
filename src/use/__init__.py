d8067d1201b44afb9d3b42b693b8b74e8a82736b
David Reilly <greyblue9@gmail.com>
2021-10-11 06:20:17 -0400

    Remove typeguard

--- src/use/__init__.py
+++ src/use/__init__.py
@@ -1,9 +1,1 @@
-try:
-    from typeguard.importhook import install_import_hook
-
-    install_import_hook(packages=["use"])
-except ImportError:
-    # typeguard not installed
-    pass
-
 from use import use
