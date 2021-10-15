5cf5a94122ae9207065768397b578452ab980033
David Reilly <greyblue9@gmail.com>
2021-10-15 05:25:42 -0400

    Revert 'Properly use database/existing install, respect explicit module name, add tar.gz test'

--- src/use/use.py
+++ src/use/use.py
@@ -804,43 +804,38 @@ def _auto_install(
             artifact_path, installation_path, module_path
         FROM distributions
         JOIN artifacts ON artifacts.id = distributions.id
-        WHERE name='{package_name}' AND version='{version!s}'
+        WHERE name='{package_name}' AND version='str(version)'
         ORDER BY artifacts.id DESC
         """
     ).fetchone()
 
-    if not query or "installation_path" not in query:
-        query = _find_or_install(name, version)
-    
+    if not query or not _ensure_path(query["artifact_path"]).exists():
+        query = _find_or_install(package_name, version)
+        
     artifact_path = _ensure_path(query["artifact_path"])
+    if _ensure_path(query["module_path"]) == Path("/data/media/0/src/use/src/twisted/__init__.py"):
+      raise BaseException("module_path")
     module_path = _ensure_path(query["module_path"])
-    
     # trying to import directly from zip
     _clean_sys_modules(rest)
     try:
         importer = zipimport.zipimporter(artifact_path)
         return importer.load_module(query["import_name"])
     except BaseException as zerr:
-        traceback.print_exc()
-    
+        pass
     orig_cwd = Path.cwd()
     mod = None
-    
+    if "installation_path" not in query:
+        query = _find_or_install(package_name, version, force_install=True)
+        artifact_path = _ensure_path(query["artifact_path"])
+        module_path = _ensure_path(query["module_path"])
     assert "installation_path" in query
     assert query["installation_path"]
     installation_path = _ensure_path(query["installation_path"])
     try:
+        module_path = _ensure_path(query["module_path"])
         os.chdir(installation_path)
-        if "/" in name:
-            import_name = name.split("/")[-1].replace(".", "/")
-            for module_path in (
-              installation_path / import_name / "__init__.py",
-              installation_path / (import_name + ".py")
-            ):
-              if module_path.exists():
-                break
-        else:
-            import_name = str(module_path.relative_to(installation_path)).replace("\\", "/").replace("/__init__.py", "").replace("-", "_")
+        import_name = str(module_path.relative_to(installation_path)).replace("\\", "/").replace("/__init__.py", "").replace("-", "_")
         return (
             mod := _load_venv_entry(
                 package_name,
@@ -1075,7 +1070,7 @@ def _find_or_install(
     python_exe = Path(sys.executable)
     env = _get_venv_env(venv_root)
     module_paths = venv_root.rglob(f"**/{relp}")
-    if force_install or not any(module_paths):
+    if force_install or (not python_exe.exists() or not any(module_paths)):
         log.info("calling pip to install install_item=%s", install_item)
 
         # If we get here, the venv/pip setup is required.
@@ -1987,7 +1982,6 @@ VALUES ({self.registry.lastrowid}, '{hash_algo.name}', '{hash_value}')"""
         _: None,  # sic! otherwise single-dispatch with 'empty' *args won't work
         /,
         *,
-        name: str,
         package_name: str = None,
         module_name: str = None,
         version: str = None,
@@ -2017,7 +2011,7 @@ VALUES ({self.registry.lastrowid}, '{hash_algo.name}', '{hash_value}')"""
         """
         log.debug(f"use-kwargs: {package_name} {module_name} {version} {hashes}")
         return self._use_package(
-            name=name,
+            name=f"{package_name}/{module_name}",
             package_name=package_name,
             module_name=module_name,
             version=version,
