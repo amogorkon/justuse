9eebcb5613171f50851c3496ee34d6210cbd9921
Anselm Kiefner <use-github@anselm.kiefner.de>
2021-10-16 16:09:58 +0200

    removed all multi-glyph chars

--- tests/.tests/.test2.py
+++ tests/.tests/.test2.py
@@ -1,18 +1,14 @@
-from beartype import beartype
-from pathlib import Path
-import os, sys
+import emoji
+from hashlib import sha256
 
-sys.path.insert(0, "")
+def represent_num_as_base(num, base):
+    if num == 0:
+        return [0]
+    digits = []
+    while num:
+        digits.append(num % base)
+        num //= base
+    return digits[::-1]
 
-cwd = Path("")
-src = (Path(__file__).parent / ".." / ".." / "src" / "use").resolve()
-print(src)
-os.chdir(src)
-from pypi_model import PyPI_Project, PyPI_Release, Version
-os.chdir(cwd)
-
-@beartype
-def test(PyPI_Project):
-    print("adsf")
-    
-test(PyPI_Release(version=Version("1")))
+E = list(emoji.EMOJI_UNICODE_ENGLISH.values())
+print(''.join(E[c] for c in represent_num_as_base(int(sha256("hello world".encode("utf8")).hexdigest(), 16), len(E))))
