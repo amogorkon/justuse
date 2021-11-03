58fab5236fdac675c7dd0bba411f71c57c5c87f1
Anselm Kiefner <use-github@anselm.kiefner.de>
2021-11-03 15:44:42 +0100

    put main classes back in main.py

--- src/use/main.py
+++ src/use/main.py
@@ -74,7 +74,7 @@ import threading
 import time
 import traceback
 from inspect import isfunction, ismethod  # for aspectizing, DO NOT REMOVE
-from logging import getLogger, root, DEBUG
+from logging import DEBUG, getLogger, root
 from pathlib import Path
 from types import ModuleType
 from typing import Any, Callable, List, Optional, Type, Union
@@ -85,7 +85,6 @@ import toml
 from beartype import beartype
 from furl import furl as URL
 from icontract import ensure, invariant, require
-
 
 from use import Hash, Modes, ModInUse, __version__, pimp
 
@@ -103,24 +102,11 @@ from icontract import require
 import use
 from use import __version__, _reloaders, _using, config, home
 from use.hash_alphabet import JACK_as_num, num_as_hexdigest
-from use.messages import (
-    AmbiguityWarning,
-    Message,
-    NotReloadableWarning,
-    NoValidationWarning,
-    UnexpectedHash,
-    VersionWarning,
-)
-from use.mod import ModuleReloader, ProxyModule
-
+from use.messages import (AmbiguityWarning, Message, NotReloadableWarning,
+                          NoValidationWarning, UnexpectedHash, VersionWarning)
 ### NEEDED FOR TESTS!! ###
-from use.pimp import (
-    _get_package_data,
-    _get_version,
-    _is_platform_compatible,
-    _is_version_satisfied,
-    get_supported,
-)
+from use.pimp import (_get_package_data, _get_version, _is_platform_compatible,
+                      _is_version_satisfied, get_supported)
 from use.pypi_model import PyPI_Project, PyPI_Release, Version
 from use.tools import methdispatch
 
@@ -149,6 +135,129 @@ class ShutdownLockReleaser:
                 reloader.stop()
         for lock in locks:
             lock.unlock()
+
+
+import asyncio
+import atexit
+import hashlib
+import threading
+import time
+import traceback
+from logging import getLogger
+from types import ModuleType
+
+from icontract import require
+
+from use.pimp import _apply_aspect, _build_mod
+
+log = getLogger(__name__)
+
+
+class ProxyModule(ModuleType):
+    def __init__(self, mod):
+        self.__implementation = mod
+        self.__condition = threading.RLock()
+
+    def __getattribute__(self, name):
+        if name in (
+            "_ProxyModule__implementation",
+            "_ProxyModule__condition",
+            "",
+            "__class__",
+            "__metaclass__",
+            "__instancecheck__",
+        ):
+            return object.__getattribute__(self, name)
+        with self.__condition:
+            return getattr(self.__implementation, name)
+
+    def __setattr__(self, name, value):
+        if name in (
+            "_ProxyModule__implementation",
+            "_ProxyModule__condition",
+        ):
+            object.__setattr__(self, name, value)
+            return
+        with self.__condition:
+            setattr(self.__implementation, name, value)
+
+    def __matmul__(self, other: tuple):
+        thing = self.__implementation
+        check, pattern, decorator = other
+        return _apply_aspect(thing, check, pattern, decorator, aspectize_dunders=False)
+
+    def __call__(self, *args, **kwargs):
+        with self.__condition:
+            return self.__implementation(*args, **kwargs)
+
+
+class ModuleReloader:
+    def __init__(self, *, proxy, name, path, initial_globals):
+        self.proxy = proxy
+        self.name = name
+        self.path = path
+        self.initial_globals = initial_globals
+        self._condition = threading.RLock()
+        self._stopped = True
+        self._thread = None
+
+    def start_async(self):
+        loop = asyncio.get_running_loop()
+        loop.create_task(self.run_async())
+
+    @require(lambda self: self._thread is None or self._thread.is_alive())
+    def start_threaded(self):
+        self._stopped = False
+        atexit.register(self.stop)
+        self._thread = threading.Thread(target=self.run_threaded, name=f"reloader__{self.name}")
+        self._thread.start()
+
+    async def run_async(self):
+        last_filehash = None
+        while not self._stopped:
+            with open(self.path, "rb") as file:
+                code = file.read()
+            current_filehash = hashlib.blake2b(code).hexdigest()
+            if current_filehash != last_filehash:
+                try:
+                    mod = _build_mod(
+                        name=self.name,
+                        code=code,
+                        initial_globals=self.initial_globals,
+                        module_path=self.path.resolve(),
+                    )
+                    self.proxy.__implementation = mod
+                except KeyError:
+                    print(traceback.format_exc())
+            last_filehash = current_filehash
+            await asyncio.sleep(1)
+
+    def run_threaded(self):
+        last_filehash = None
+        while not self._stopped:
+            with self._condition:
+                with open(self.path, "rb") as file:
+                    code = file.read()
+                current_filehash = hashlib.blake2b(code).hexdigest()
+                if current_filehash != last_filehash:
+                    try:
+                        mod = _build_mod(
+                            name=self.name,
+                            code=code,
+                            initial_globals=self.initial_globals,
+                            module_path=self.path,
+                        )
+                        self.proxy._ProxyModule__implementation = mod
+                    except KeyError:
+                        print(traceback.format_exc())
+                last_filehash = current_filehash
+            time.sleep(1)
+
+    def stop(self):
+        self._stopped = True
+
+    def __del__(self):
+        self.stop()
 
 
 class Use(ModuleType):
@@ -335,7 +444,9 @@ CREATE TABLE IF NOT EXISTS "depends_on" (
         for name, version, artifact_path, installation_path in self.registry.execute(
             "SELECT name, version, artifact_path, installation_path FROM distributions JOIN artifacts on distributions.id = distribution_id"
         ).fetchall():
-            if not (pimp._ensure_path(artifact_path).exists() and pimp._ensure_path(installation_path).exists()):
+            if not (
+                pimp._ensure_path(artifact_path).exists() and pimp._ensure_path(installation_path).exists()
+            ):
                 self.del_entry(name, version)
         self.registry.connection.commit()
 
@@ -486,7 +597,9 @@ VALUES ({self.registry.lastrowid}, '{hash_algo.name}', '{hash_value}')"""
                 # we're in jupyter, we use the CWD as set in the notebook
                 if not jupyter and hasattr(main_mod, "__file__"):
                     source_dir = (
-                        pimp._ensure_path(inspect.currentframe().f_back.f_back.f_code.co_filename).resolve().parent
+                        pimp._ensure_path(inspect.currentframe().f_back.f_back.f_code.co_filename)
+                        .resolve()
+                        .parent
                     )
             if source_dir is None:
                 if main_mod.__loader__ and hasattr(main_mod.__loader__, "path"):
