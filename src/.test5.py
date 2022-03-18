9b0f5ae7a42b36664b2aac803f86c8c28c6ff78a
Anselm Kiefner <use-github@anselm.kiefner.de>
2022-03-18 14:42:42 +0100

    Detail in aspects_dry_run

--- src/.test5.py
+++ src/.test5.py
@@ -5,26 +5,7 @@ import numpy
 import use
 from use.aspectizing import _unwrap, _wrap
 
-
-def f(x):
-    return x**2
-
-
-def test_decorate(reuse):
-    def decorator(func):
-        def wrapper(x):
-            return func(x + 1)
-
-        return wrapper
-
-    _wrap(thing=sys.modules[__name__], obj=f, decorator=decorator, name="f")
-    assert f(3) == 16
-    _unwrap(thing=sys.modules[__name__], name="f")
-    assert f(3) == 9
-
-
-test_decorate(use)
-# numpy @ use
+numpy @ use
 mod = use(use.Path(".test4.py"))
 
 
