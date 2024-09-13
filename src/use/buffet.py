d833a58abcda1a0927c0d773755cb7029a0a16b1
Anselm Kiefner <use-github@anselm.kiefner.de>
2024-09-13 17:39:19 +0200

    Message.cant_import only takes kwargs, like everything else

--- src/use/buffet.py
+++ src/use/buffet.py
@@ -34,5 +34,5 @@ def buffet_table(case, kwargs):
         case 1, 0, _, 1: return _pebkac_no_hash(**kwargs) # noqa: E701
         case 0, 1, _, 1: return _pebkac_no_version(**kwargs) # noqa: E701
         case 0, _, 1, 0: return _import_public_no_install(**kwargs) # noqa: E701
-        case _, _, 0, 0: return ImportError(Message.cant_import(kwargs["pkg_name"]))  # noqa: E701
+        case _, _, 0, 0: return ImportError(Message.cant_import(**kwargs))  # noqa: E701
 # fmt: on
