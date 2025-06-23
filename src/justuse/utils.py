d7a7cf1556e7d51f48e7f6c7c65a490316bd8ed2
Anselm Kiefner <use-github@anselm.kiefner.de>
2025-06-23 07:00:19 +0200

    tools are now in utils

--- src/justuse/utils.py
+++ src/justuse/utils.py
@@ -1,28 +1,17 @@
-from datetime import datetime
-
-def excel_style_datetime(now: datetime) -> float:
-    """
-    Build a float representing the current time in the excel format.
-    First 4 digits are the year, the next two are the month, the next two are the day followed
-    by a decimal point, then time in fraction of the day.
-    Args:
-        now (datetime): datetime instance to be converted
-    Returns:
-        float: Excel style datetime
-    """
-    return int(f"{now.year:04d}{now.month:02d}{now.day:02d}") + round(
-        (now.hour * 3600 + now.minute * 60 + now.second) / 86400, 6
-    )
 """
 Configuration, constants, and utility functions for justuse.
 """
 
 import hashlib
+import inspect
 import os
+import re
 import tempfile
+from collections.abc import Callable
 from datetime import datetime, timezone
 from enum import Enum, Flag, IntEnum, auto
 from pathlib import Path
+from typing import Any
 from uuid import uuid4
 
 # Home directory for justuse
@@ -96,3 +85,86 @@ class ModeFlags(Flag):
     NO_CLEANUP,
     NO_BROWSER,
 ) = ModeFlags
+
+
+def excel_style_datetime(now: datetime) -> float:
+    """
+    Build a float representing the current time in the excel format.
+    First 4 digits are the year, the next two are the month, the next two are the day followed
+    by a decimal point, then time in fraction of the day.
+    Args:
+        now (datetime): datetime instance to be converted
+    Returns:
+        float: Excel style datetime
+    """
+    return int(f"{now.year:04d}{now.month:02d}{now.day:02d}") + round(
+        (now.hour * 3600 + now.minute * 60 + now.second) / 86400, 6
+    )
+
+
+def _is_callable(thing):
+    try:
+        return callable(
+            object.__getattribute__(type(thing), "__call__")
+        )  # to even catch weird non-callable __call__ methods
+    except AttributeError:
+        return False
+
+
+class ALL(Enum):
+    methods = inspect.ismethod
+    properties = inspect.isdatadescriptor
+    functions = inspect.isfunction
+    classes = inspect.isclass
+
+
+def apply(
+    decorator: Callable,
+    kind: ALL,
+    /,
+    check=_is_callable,
+    pattern="",
+    mode: ModeFlags = None,
+):
+    if mode is None:
+        mode = DEFAULT
+
+    def sugar(*things: list[Any]):
+        visited = {id(obj) for obj in vars(object).values()}
+        visited.add(id(type))
+        for thing in things:
+            if id(thing) in visited:
+                continue
+            for name in dir(thing):
+                obj = getattr(thing, name, None)
+                qualname = getattr(obj, "__qualname__", name)
+
+                if (
+                    obj is None
+                    or id(obj) in visited
+                    or not check(obj)
+                    or not kind(obj)
+                    or not re.match(pattern, name)
+                    or (
+                        INC_DUNDER in mode
+                        and name.startswith("__")
+                        and name.endswith("__")
+                    )
+                ):
+                    continue
+
+                try:
+                    wrapped = decorator(obj)
+                    setattr(thing, name, wrapped)
+                except BaseException as exc:
+                    if VERBOSE in mode:
+                        print(
+                            f"Failed to apply decorator {decorator} to {qualname}: {exc}"
+                        )
+                    wrapped = obj
+                visited.add(id(wrapped))
+                if VERBOSE in mode:
+                    print(f"Applied decorator {decorator} to {qualname}")
+        return things[0] if len(things) == 1 else things
+
+    return sugar
